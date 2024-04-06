0e2e45bfc737784d68faa52f2f3bb550b5fc7b03
NikSidorov <140190553+NikSidorov@users.noreply.github.com>
2024-04-06 19:43:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import telebot
 
-bot = telebot.TeleBot("6365081423:AAH_fHYAn39q93QTnjRipfJqbxLElZZrcs8")
+bot = telebot.TeleBot("6365081423:AAH_fHYAn39q93QTnjRipfJqbxLElZZrc")
 
 API_openweathermap = '117dda706b02b9450d2bfeea6d46ac3c'
